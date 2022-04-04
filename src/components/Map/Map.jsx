30be5745d5ef52e96e57dfe8660e30f24233259a
mohamedhassan94 <mo.ref.has94@gmail.com>
2022-04-05 00:08:07 +0200

    Update google maps API key

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -14,7 +14,7 @@ const Map = ({coordinates, setCoordinates, setBounds, places, setChildClicked}) 
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+                bootstrapURLKeys={{ key: "AIzaSyDL3txJKLWEyhqMCPccJ43hNs_LvvEfri4" }}
                 defaultCenter={coordinates}
                 center={coordinates}
                 defaultZoom={14}
